cb87dec313f1b4763f008f55765cb066ef157151
Callan Gray <00105058@uwa.edu.au>
2021-01-19 16:52:59 +0800

    cleanup

--- src/icrar/leap-accelerate/model/cpu/MetaData.cc
+++ src/icrar/leap-accelerate/model/cpu/MetaData.cc
@@ -50,6 +50,8 @@ namespace cpu
         m_constants.nbaselines = ms.GetNumBaselines();
         m_constants.referenceAntenna = refAnt ? refAnt.get() : ms.GetTotalAntennas() - 1;
 
+        std::cout << m_constants.referenceAntenna << std::endl;
+
         m_constants.channels = 0;
         m_constants.freq_start_hz = 0;
         m_constants.freq_inc_hz = 0;
@@ -190,7 +192,7 @@ namespace cpu
         0, std::cos(ang3), -std::sin(ang3),
         0, std::sin(ang3),  std::cos(ang3);
 
-        // TODO(calgray) Alternatively calc only the three vec
+        // Alternatively calculate only the three vec
         // m_lmn = Eigen::Vector3d();
         // m_lmn(0) = std::cos(polar_direction(1)) * std::sin(-m_constants.dlm_ra);
         // m_lmn(1) = std::sin(polar_direction(1)) * std::cos(m_constants.phase_centre_ra_rad) - std::cos(polar_direction(1)) * std::cos(m_constants.phase_centre_dec_rad) * std::sin(-m_constants.dlm_ra);
@@ -207,9 +209,8 @@ namespace cpu
     void MetaData::SetDirection(const SphericalDirection& direction)
     {
         m_direction = direction;
-
-        m_constants.dlm_ra = direction(0) - m_constants.phase_centre_ra_rad; //TODO: dlm_ra is not a constant
-        m_constants.dlm_dec = direction(1) - m_constants.phase_centre_dec_rad; //TODO: dlm_dec is not a constant
+        m_constants.dlm_ra = direction(0) - m_constants.phase_centre_ra_rad; //TODO(cgray): dlm_ra is not a constant
+        m_constants.dlm_dec = direction(1) - m_constants.phase_centre_dec_rad; //TODO(cgray): dlm_dec is not a constant
         
         m_dd = GenerateDDMatrix(direction);
         LOG(trace) << "dd: " << pretty_matrix(m_dd);
