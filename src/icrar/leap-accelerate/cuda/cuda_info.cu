73d924ff56cae9e8a503754540a820827a1f5556
Callan Gray <00105058@uwa.edu.au>
2021-03-30 18:07:17 +0800

    cleanup

--- src/icrar/leap-accelerate/cuda/cuda_info.cu
+++ src/icrar/leap-accelerate/cuda/cuda_info.cu
@@ -35,11 +35,6 @@ int GetCudaDeviceCount()
     return deviceCount;
 }
 
-// __global__ void CheckSM()
-// {
-//     __CUDA_ARCH__
-// }
-
 void printCudaVersion()
 {
     std::cout << "CUDA Compiled version: " << __CUDACC_VER_MAJOR__ << __CUDACC_VER_MINOR__ << __CUDACC_VER_BUILD__ << std::endl;
