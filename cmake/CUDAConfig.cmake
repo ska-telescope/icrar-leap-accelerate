15b606a4cf32f5f224c9ea3778b446a5acf2d8a0
Callan Gray <00105058@uwa.edu.au>
2021-03-31 11:37:09 +0800

    typo

--- cmake/CUDAConfig.cmake
+++ cmake/CUDAConfig.cmake
@@ -88,6 +88,5 @@ function(configure_cuda_warnings TARGET_NAME)
     # 3057 annotation is ignored on a function that is explicitly defaulted
     # 2929 annotation is ignored on a function that is explicitly defaulted
     target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:-Xcudafe="--diag_suppress=3057,2929">)
-    else()
   endif()
 endfunction()
