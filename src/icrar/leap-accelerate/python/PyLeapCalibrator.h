16987183d58940ebdf8966a41fecece08978233c
Callan Gray <00105058@uwa.edu.au>
2021-05-27 13:33:13 +0800

    python binding working

--- src/icrar/leap-accelerate/python/PyLeapCalibrator.h
+++ src/icrar/leap-accelerate/python/PyLeapCalibrator.h
@@ -4,6 +4,7 @@
 #include <icrar/leap-accelerate/algorithm/ILeapCalibrator.h>
 #include <icrar/leap-accelerate/algorithm/LeapCalibratorFactory.h>
 #include <boost/python.hpp>
+#include <iostream>
 
 namespace icrar
 {
@@ -13,42 +14,53 @@ namespace python
      * @brief An adapter to a boost::python compatible class
      * @note the Calibrate signature may change as needed
      */
-    class PyLeapCalibrator : public ILeapCalibrator
+    class PyLeapCalibrator // : public ILeapCalibrator
     {
         std::unique_ptr<ILeapCalibrator> m_calibrator;
     public:
-        PythonLeapCalibrator(ComputeImplementation impl)
+        PyLeapCalibrator(ComputeImplementation impl)
         {
             m_calibrator = LeapCalibratorFactory::Create(impl);
         }
 
-        void Calibrate(
-            std::function<void(const cpu::Calibration&)> outputCallback,
-            const icrar::MeasurementSet& ms,
-            const std::vector<SphericalDirection>& directions,
-            const Slice& solutionInterval,
-            double minimumBaselineThreshold,
-            boost::optional<unsigned int> referenceAntenna,
-            const ComputeOptionsDTO& computeOptions) override
+        PyLeapCalibrator(const PyLeapCalibrator& other) {}
+
+        void Hello()
         {
-            m_calibrator(
-                outputCallback,
-                ms,
-                directions,
-                solutionInterval,
-                minimumBaselineThreshold,
-                referenceAntenna,
-                computeOptions);
+            std::cout << "hello world" << std::endl;
         }
-    }
-}
-}
 
-using namespace boost::python;
+        // void Calibrate(
+        //     std::function<void(const cpu::Calibration&)> outputCallback,
+        //     const icrar::MeasurementSet& ms,
+        //     const std::vector<SphericalDirection>& directions,
+        //     const Slice& solutionInterval,
+        //     double minimumBaselineThreshold,
+        //     boost::optional<unsigned int> referenceAntenna,
+        //     const ComputeOptionsDTO& computeOptions) override
+        // {
+        //     m_calibrator->Calibrate(
+        //         outputCallback,
+        //         ms,
+        //         directions,
+        //         solutionInterval,
+        //         minimumBaselineThreshold,
+        //         referenceAntenna,
+        //         computeOptions);
+        // }
+    };
+} // namespace python
+} // namespace icrar
 
-BOOST_PYTHON_MODULE("leap_accelerate")
+
+BOOST_PYTHON_MODULE(leap_accelerate)
 {
-    boost::python::class_<icrar::python::PythonLeapCalibrator>("LeapCalibrator")
-        .def("calibrate", icrar::python::PythonLeapCalibrator::Calibrate);
+    boost::python::class_<icrar::python::PyLeapCalibrator>("LeapCalibrator", boost::python::init<icrar::ComputeImplementation>())
+        .def("hello",     &icrar::python::PyLeapCalibrator::Hello);
+        //.def("calibrate", &icrar::python::PyLeapCalibrator::Calibrate);
+
+    boost::python::enum_<icrar::ComputeImplementation>("ComputeImplementation")
+        .value("cpu", icrar::ComputeImplementation::cpu)
+        .value("cuda", icrar::ComputeImplementation::cuda);
 }
 
